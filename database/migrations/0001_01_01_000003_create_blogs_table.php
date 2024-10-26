5d4186aa3bfe8c1ed2742ff7dc370a826c170b42
ankitmishra42 <ankitmishra6221@gmail.com>
2024-10-27 01:34:28 +0530

    Admin & Blog Models are commited

--- database/migrations/0001_01_01_000003_create_blogs_table.php
+++ database/migrations/0001_01_01_000003_create_blogs_table.php
@@ -11,21 +11,14 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('admins', function (Blueprint $table) {
+        Schema::create('blogs', function (Blueprint $table) {
             $table->id();
             $table->string('name');
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
+            $table->rememberToken();
             $table->timestamps();
         });
-    }
-
-    /**
-     * Reverse the migrations.
-     */
-    public function down(): void
-    {
-        Schema::dropIfExists('users');
     }
 };
