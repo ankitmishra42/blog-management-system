4dfc59786aff9dc6687cf19413fc2dc4bf900101
ankitmishra42 <ankitmishra6221@gmail.com>
2024-10-27 15:46:10 +0530

    admin-create-blogs, admin-view-blogs, and admin-delete-blogs file added

--- routes/web.php
+++ routes/web.php
@@ -4,9 +4,6 @@ use App\Http\Controllers\AdminController;
 use App\Http\Controllers\UserController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
-});
 
 // User Controller Part
 Route::get('/blogs', [UserController::class, 'Blogs']);
